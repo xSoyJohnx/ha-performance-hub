ed384e79f11d8eab50f2f167b7c8f32e36e35c41
John <maicoljohn940@gmail.com>
2026-04-22 18:31:10 -0500

    fix tanstack vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    react(),
+  ],
 })
